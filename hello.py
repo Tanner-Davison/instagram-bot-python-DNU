4e93b5fa0ef88c88d0f4f7105220016228e90d0f
Tanner Davison <tanner.davison95@gmail.com>
2024-06-19 23:56:08 -0600

    just messing around and shiz

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 import numpy as np
 
-msg = "Roll a dice"
-print(msg)
 
-print(np.random.randint(1,9))
+name= input('what is your name?\n')
+print('your name has ' + str(len(name)) + ' Characters in it')
+randomNumber = np.random.randint(1,9)
+print(randomNumber)
+#ask for the users name
+print('Hello, What is the name of the city you grew up in?')
+cityName = input()
+print('What is the name of your pet? Or a friends pet?')
+petsName = input()
+#return a response 
+print('You could call your band ' + cityName + ' ' +petsName)
+
